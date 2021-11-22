a802483b25cdd1e251199dcb0a1b93c37229f95e
maojunsen <maojunsen@richonn.com>
2021-11-22 11:11:35 +0800

    data binding

---
+++ pages/demo03/demo03.js
@@ -0,0 +1,15 @@
+//Page Object
+Page({
+    data: {
+        msg: "hello mina",
+        num: 100000,
+        isGirl: false,
+        person: {
+            age: 74,
+            height: 145,
+            weight: 200,
+            name: "富婆"
+        },
+        isChecked: false
+    },
+});
