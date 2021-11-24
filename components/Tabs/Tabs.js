d8c56dcb19ca03f9b8787fa9b355361c3e2a4425
JohnsonMori <960612296@qq.com>
2021-11-24 17:29:17 +0800

    feat: child pass to parent

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -28,20 +28,25 @@ Component({
              * 4 对数组循环
              *  1 给每一个循环项 选中属性 改为 false
              *  2 给 当前的索引的 项 添加激活选中效果就可以了！！！
+             * 5 点击事件触发的时候
+             *    触发父组件中的自定义事件 同时传递数据给 父组件
+             *    this.triggerEvent("父组件自定义事件的名称", 要传递的参数)
              */
             // 2 获取索引
             const { index } = e.currentTarget.dataset;
+            this.triggerEvent("itemchange", { index })
+            // 5 触发父组件中的自定义事件 同时传递数据给
             // 3 获取data中的数组
             // let { tabs } = this.data;
-            let tabs = JSON.parse(JSON.stringify(this.data.tabs))
+            // let tabs = JSON.parse(JSON.stringify(this.data.tabs))
             // 4 循环数组
             // [].foreach 遍历数组 遍历数组的时候 修改了 v，也会导致源数组被修改
-            tabs.forEach((v, i) => {
-                v.isActive = i === index;
-            });
-            this.setData({
-                tabs
-            });
+            // tabs.forEach((v, i) => {
+            // v.isActive = i === index;
+            // });
+            // this.setData({
+            // tabs
+            // });
         }
     }
 })
--- pages/demo17/demo17.js
+++ pages/demo17/demo17.js
@@ -29,59 +29,14 @@ Page({
         ]
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    // 自定义事件 用来接收子组件传递的数据的
+    handleItemChange(e) {
+        const { tabs } = this.data;
+        tabs.forEach((v, i) => {
+            v.isActive = i === e.detail.index;
+        });
+        this.setData({
+            tabs
+        });
     }
 })
