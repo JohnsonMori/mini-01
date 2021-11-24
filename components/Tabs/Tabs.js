344c9fafc7b9bf220941e7a30005a9eeec564ceb
JohnsonMori <960612296@qq.com>
2021-11-24 17:02:13 +0800

    feat: parent pass to child

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,36 +4,16 @@ Component({
      * 组件的属性列表
      */
     properties: {
-
+        tabs: {
+            type: Array,
+            value: []
+        }
     },
 
     /**
      * 组件的初始数据
      */
-    data: {
-        tabs: [
-            {
-                id: 0,
-                name: "首页",
-                isActive: true
-            },
-            {
-                id: 1,
-                name: "原创",
-                isActive: false
-            },
-            {
-                id: 2,
-                name: "分类",
-                isActive: false
-            },
-            {
-                id: 3,
-                name: "关于",
-                isActive: false
-            }
-        ]
-    },
+    data: {},
 
     /**
      * 1 页面.js 文件中 存放事件回调函数的时候 存放在data同层级下！！！
